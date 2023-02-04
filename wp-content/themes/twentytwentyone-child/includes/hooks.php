70a4201b651f7627234e6d3d3875ffd8574c4630
A.Horban <gunner14th@gmail.com>
2023-02-04 13:33:21 +0200

    PHPMailer config

--- wp-content/themes/twentytwentyone-child/includes/hooks.php
+++ wp-content/themes/twentytwentyone-child/includes/hooks.php
@@ -92,3 +92,19 @@ function send_new_post_to_author_func( $post_author_email, $post_title, $post_ur
     $message .= $post_title . ": " . $post_url . "\n";
     wp_mail( $post_author_email, $subject, $message );
 }
+
+/**
+ * PHPMailer config
+ */
+function rwtz_phpmailer_smtp( $phpmailer ) {
+    $phpmailer->isSMTP();
+    $phpmailer->Host = SMTP_server;
+    $phpmailer->SMTPAuth = SMTP_AUTH;
+    $phpmailer->Port = SMTP_PORT;
+    $phpmailer->Username = SMTP_username;
+    $phpmailer->Password = SMTP_password;
+    $phpmailer->SMTPSecure = SMTP_SECURE;
+    $phpmailer->From = SMTP_FROM;
+    $phpmailer->FromName = SMTP_NAME;
+}
+add_action( 'phpmailer_init', 'rwtz_phpmailer_smtp' );
